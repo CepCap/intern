97ae740b63b97fd3950f07ac94bc57f2e0917b98
CepCap <nikinoob@gmail.com>
2019-07-05 00:10:49 +0300

    Homework

---
+++ work/numbersgenerator/numbers.rb
@@ -0,0 +1,35 @@
+def uniqate(array)
+  array.inject([]) { |uniq, el| uniq << el unless uniq.include? el; uniq }
+end
+
+def numbers_generator(file, amount)
+  File.open(file, 'wb') do |f|
+    amount.times do
+      f.print (rand(10 ** 4).to_s + ' ')
+    end
+  end
+end
+
+def has_dups?(file)
+  File.read(file).split(' ').inject([]) do |uniq, el|
+    return true if uniq.include? el
+    uniq << el
+  end
+  false
+end
+
+def no_dups(file)
+  uniqate(File.read(file).split(' ')).each do |el|
+    File.open(file + '_no_dups', 'wb') do |f|
+      f.print el.to_s + ' '
+    end
+  end
+end
+
+p uniqate([6,6,6,6,6]) == [6]
+p uniqate([6,7,8,6,6]) == [6,7,8]
+filename = 'generated_numbers'
+numbers_generator(filename, 10*7)
+p has_dups?(filename)
+no_dups(filename)
+p has_dups?("#{filename}_no_dups") == false
